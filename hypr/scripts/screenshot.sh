5a39dda52df6ae8fbb98ee635ce1efbd48c4ce99
sejjy <sejjymvm@gmail.com>
2025-12-08 21:40:30 +0800

    fix: improve filename generation

--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -1,29 +1,50 @@
 #!/usr/bin/env bash
+#
+# Take a screenshot of the target passed
+#
+# Author: Jesse Mirabel <sejjymvm@gmail.com>
+# Created: January 2, 2025
+# License: MIT
 
-DIR=~/Pictures/Screenshots
+SDIR=$HOME/Pictures/Screenshots
+
+if [[ ! -d $SDIR ]]; then
+	mkdir -p "$SDIR"
+fi
+
+print-usage() {
+	cat <<- EOF
+		USAGE: ${0##*/} [TARGET]
+
+		Take a screenshot of the target passed
+
+		TARGETS:
+		    active    Currently active window
+		    area      Manually select a rectangular region or window
+		    output    Currently active output
+		    screen    Entire screen including all visible outputs
+	EOF
+	exit 1
+}
 
 main() {
+	local name
+	name="Screenshot_$(date +%Y%m%d_%H%M%S_%2N).png"
+	local file="$SDIR/$name"
+
 	local target=$1
+	case $target in
+		'area') grimblast --freeze copysave "$target" "$file" ;;
+		'active' | 'output' | 'screen') grimblast copysave "$target" "$file" ;;
+		*) print-usage ;;
+	esac
 
-	local i=1
-	while [[ -e "$DIR/Screenshot ($i).png" ]]; do
-		((i++))
-	done
-
-	local name="Screenshot ($i).png"
-	local file="$DIR/$name"
-
-	if [[ $target == 'area' ]]; then
-		grimblast --freeze copysave "$target" "$file"
-	else
-		grimblast copysave "$target" "$file"
+	if [[ ! -s $file ]]; then
+		rm -f "$file"
+		exit 1
 	fi
 
-	if [[ -s $file ]]; then
-		notify-send "$name saved in $DIR" -i "$file"
-	else
-		rm "$file"
-	fi
+	notify-send "$name saved in $SDIR" -i "$file"
 }
 
 main "$@"
