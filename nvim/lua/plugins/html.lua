11e307417570b9bb90009bace10d964a1211d1b7
sejjy <sejjymvm@gmail.com>
2026-01-08 08:42:00 +0800

    fix(nvim-ts-autotag): update spec

--- nvim/lua/plugins/html.lua
+++ nvim/lua/plugins/html.lua
@@ -1,7 +1,7 @@
 return {
 	"windwp/nvim-ts-autotag",
 
-	event = "InsertEnter",
+	ft = { "html", "javascriptreact", "typescriptreact" },
 
 	config = function()
 		require("nvim-ts-autotag").setup()
