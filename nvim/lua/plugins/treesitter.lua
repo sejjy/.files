20bc47649af183997f69d82c41f2e928a3d22c86
sejjy <sejjymvm@gmail.com>
2026-01-08 08:44:03 +0800

    feat(nvim-treesitter): migrate to main branch

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,44 +1,48 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 
+	branch = "main",
+
+	lazy = false,
+
 	build = ":TSUpdate",
 
-	event = "BufReadPre",
-
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"bash",
-				"c",
-				"css",
-				"diff",
-				"gitignore",
-				"html",
-				"javascript",
-				"json",
-				"jsonc",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"query",
-				"regex",
-				"rust",
-				"typescript",
-				"vim",
-				"vimdoc",
-			},
+		require("nvim-treesitter").setup({
+			install_dir = vim.fn.stdpath("data") .. "/site",
+		})
 
-			sync_install = false,
-			auto_install = true,
+		local parsers = {
+			"bash",
+			"c",
+			"css",
+			"diff",
+			"git_config",
+			"git_rebase",
+			"gitattributes",
+			"gitcommit",
+			"gitignore",
+			"html",
+			"javascript",
+			"json",
+			"lua",
+			"make",
+			"markdown",
+			"python",
+			"query",
+			"regex",
+			"rust",
+			"tsx",
+			"typescript",
+			"vim",
+		}
 
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-
-			indent = { enable = true },
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "LazyDone",
+			once = true,
+			callback = function()
+				require("nvim-treesitter").install(parsers)
+			end,
 		})
 	end,
 }
